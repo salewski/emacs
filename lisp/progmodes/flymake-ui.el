12aa247b76e662f74d7289910fe759427cbd203f
João Távora <joaotavora@gmail.com>
2017-09-20 19:10:30 +0100

    Protect against timer triggers when no flymake-mode
    Not 100% sure on how to trigger this error, but it happened once or
    twice, possibly during heavy debugging scenarios that wouldn't really
    happen.  But a good idea anyway.
    
    * lisp/progmodes/flymake-ui.el (flymake-on-timer-event): Check flymake-mode

--- lisp/progmodes/flymake-ui.el
+++ lisp/progmodes/flymake-ui.el
@@ -331,7 +331,8 @@ with flymake-specific meaning can also be used.
   "Start a syntax check for buffer BUFFER if necessary."
   (when (buffer-live-p buffer)
     (with-current-buffer buffer
-      (when (and (not flymake-is-running)
+      (when (and flymake-mode
+                 (not flymake-is-running)
 		 flymake-last-change-time
 		 (> (- (float-time) flymake-last-change-time)
                     flymake-no-changes-timeout))
